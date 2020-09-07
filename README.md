5a5a8e4d148e1f8eb1cc04bf2772b1d5863a1bca
ShNorah <aksharon018@gmail.com>
2020-09-07 22:44:55 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# uniquekids
-this is the frontend of the unique kids site developed in reactjs
---
+++ precache-manifest.347af88159db6f3d1c0e824f7f6cef99.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5180d5a3ab8f2970c6f1529e40e9c0d6",
+    "url": "/uniquekids/index.html"
+  },
+  {
+    "revision": "edc5d3f083bbb6b0d448",
+    "url": "/uniquekids/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bc987783489e8f676bb4",
+    "url": "/uniquekids/static/css/main.1c75bc72.chunk.css"
+  },
+  {
+    "revision": "edc5d3f083bbb6b0d448",
+    "url": "/uniquekids/static/js/2.7887b4ba.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/uniquekids/static/js/2.7887b4ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc987783489e8f676bb4",
+    "url": "/uniquekids/static/js/main.b9fd3cb8.chunk.js"
+  },
+  {
+    "revision": "c0bda70e79b18110c6e0",
+    "url": "/uniquekids/static/js/runtime-main.95f4c2cd.js"
+  },
+  {
+    "revision": "600be4fa6163e8fae15785a15c1d4243",
+    "url": "/uniquekids/static/media/banner.600be4fa.jpg"
+  },
+  {
+    "revision": "46cff4d421535d024fedb3940114b1af",
+    "url": "/uniquekids/static/media/banner1.46cff4d4.jpg"
+  },
+  {
+    "revision": "afd2bf90a137e422e6ba0c72004cd87f",
+    "url": "/uniquekids/static/media/banner2.afd2bf90.jpg"
+  }
+]);
